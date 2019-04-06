259549ea1648da97ca1d1ec8bf49c742664eefbd
phil <phil@hattwick.com>
2019-04-06 13:35:58 -0400

    combined variations on fibonacci solution into single path

--- classic/fib-tests.py
+++ classic/fib-tests.py
@@ -1,6 +1,10 @@
 # Classic Problems with Python (2019 Manning Press)
 # Scratch Tests while skimming the book
 
+from typing import Dict
+from functools import lru_cache
+
+memo: Dict[int, int] = {0: 0, 1: 1}  # our base cases for fib3
 
 def fib1(n: int) -> int:
     # Results in Max recursion error
@@ -8,6 +12,48 @@ def fib1(n: int) -> int:
     return fib1(n - 1) + fib1(n - 2)
 
 
-if __name__ == "__main__":
-    print(fib1(5))
+def fib2(n: int) -> int:
+    #todo: add counter to track # of function calls
+    if n < 2:  # base case to avoid excessive recursion error
+        return n
+    return fib2(n - 2) + fib2(n - 1)  # recursive case
 
+
+def fib3(n: int) -> int:
+    if n not in memo:
+        memo[n] = fib3(n - 1) + fib3(n - 2)  # memoization
+    return memo[n]
+
+
+@lru_cache(maxsize=None)
+def fib4(n: int) -> int:  # same definition as fib2()  Decorator caches each return value
+    if n < 2:  # base case
+        return n
+    return fib4(n - 2) + fib4(n - 1)  # recursive case
+
+
+def fib5(n: int) -> int:
+    if n == 0: return n  # special case
+    last: int = 0  # initially set to fib(0)
+    next: int = 1  # initially set to fib(1)
+    for _ in range(1, n):
+        last, next = next, last + next
+    return next
+
+
+if __name__ == "__main__":
+    print('\nfib2 approach')
+    print(fib2(5))
+    print(fib2(30))  # takes a few seconds to run these 50000+ calls
+
+    print('\nfib3 approach')
+    print(fib3(5))
+    print(fib3(50))
+
+    print('\nfib4 approach')
+    print(fib4(5))
+    print(fib4(50))
+
+    print('\nfib5 approach')
+    print(fib5(5))
+    print(fib5(50))
